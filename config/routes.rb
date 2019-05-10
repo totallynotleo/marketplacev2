80ef4a497790990b99704cd3a85d8c7b794fe981
TSaker95 <torryarno@hotmail.com>
2019-05-10 11:57:02 +1000

    search feature complete! i hope

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
   post "/orders/add_listing/:listing_id", to: 'orders#add_listing', as: 'order_add_listing'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'listings#index'
+  get '/search' => 'listings#search', :as => 'search_page'
 end
