65d0a3b1a94fc986d115b518c862a41f273a4dde
totallynotleo <leodabrim@gmail.com>
2019-05-11 10:11:49 +1000

    Recomitting

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,5 @@ Rails.application.routes.draw do
   post 'add_item/:listing_id', to: 'carts#add_item', as: 'add_item'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'listings#index'
+  get '/search' => 'listings#search', :as => 'search_page'
 end
